0a8a7c74708aae973f10e5c0d1c88687dc741d50
FinnStokes <finn.stokes@gmail.com>
2014-04-28 19:59:40 +0930

    Removed patterns from ocean layers

--- world.py
+++ world.py
@@ -3,15 +3,12 @@ import bisect
 import pygame
 from pygame.locals import *
 
-import resources
-
 class Layers():
     def __init__(self, densities, depths):
         assert(len(densities) == len(depths))
         self.densities = densities
         self.depths = depths
-        self.colors = [pygame.Color('cyan'), pygame.Color('red'), pygame.Color('yellow'), pygame.Color('green'), pygame.Color('blue')]
-        self.filenames = ["RedRipple.png","YellowDiamonds.png","GreenStripe.png","BlueTartan.png"]
+        self.colors = [pygame.Color('cyan'), pygame.Color('red'), pygame.Color('yellow'), pygame.Color('#008000'), pygame.Color('blue')]
 
     def __len__(self):
         return len(self.densities)
@@ -42,12 +39,4 @@ class Layers():
         for i in xrange(1,len(self.depths)):
             rect.height = self.depths[i] - self.depths[i-1]
             rect.top = self.depths[i-1]
-            tile,square = resources.load_png(self.filenames[i-1])
-            numtilesY = rect.height/square.height+1
-            numtilesX = rect.width/square.width+1
-
-            for jy in xrange(0,numtilesY):
-                for jx in xrange(0,numtilesX):
-                    xplace = jx*86
-                    yplace = rect.top + jy*86
-                    surface.blit(tile,(xplace,yplace))
+            pygame.draw.rect(surface, self.colors[i%len(self.colors)], rect)
